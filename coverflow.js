02dd993be6713e29eb83c7b50d463c87a7a1d6e7
mhamdan95 <36275074+mhamdan95@users.noreply.github.com>
2018-12-31 14:46:04 +0100

    Update coverflow.js
    J'ai créé deux instances du coverflow

--- coverflow.js
+++ coverflow.js
@@ -1,1 +1,40 @@
-function
+/*!
+ * JavaScript coverflow.
+ *
+ * @by Théo Hamdan
+ * @version 1.0
+ */
+
+(function($)
+{
+	
+    $.fn.coverflow=function(elementID)
+    {
+       //Initialisation du plugin
+	   var divID = '#' + elementID;
+	   initCoverflow(divID);
+	   handleEvt(divID);
+    };
+})(jQuery);
+
+/**
+* Cette fonction va initialiser le plugin
+*/
+function initCoverflow(elementID)
+{
+	//Création du bloc Coverflow
+    $('<div id="coverflow-controls">').appendTo(elementID);
+	
+	//Ajout des controles (stop/play, next ...)
+	$('<button class="coverflow-control-elmt stopestart">Démarrer</button>').appendTo(elementID + " #coverflow-controls");
+}
+ 
+/**
+* Liaison avec les evenements
+*/
+function handleEvt(elementID)
+{
+    $(elementID + ' .stopestart').on('click', function () {
+        alert("play/stop on instance " + elementID);
+    });
+}
