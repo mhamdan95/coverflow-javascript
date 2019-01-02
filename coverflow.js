3a145f67044ec29e0340d036e9bac172d086e85a
mhamdan95 <36275074+mhamdan95@users.noreply.github.com>
2019-01-02 19:52:41 +0100

    Update coverflow.js

--- coverflow.js
+++ coverflow.js
@@ -1,40 +1,58 @@
 /*!
  * JavaScript coverflow.
  *
- * @by Théo Hamdan
+ * @by Théo Mahmoud Hamdan
  * @version 1.0
  */
 
-(function($)
-{
-	
-    $.fn.coverflow=function(elementID)
+(function($){  
+    $.fn.numbertext=function(elementID)
     {
        //Initialisation du plugin
-	   var divID = '#' + elementID;
-	   initCoverflow(divID);
-	   handleEvt(divID);
+     var divID = '#' + elementID;
+     plusSlides(divID);
+     currentSlide(divID);
+     showSlides(divID);
+     initCoverflow(divID);
     };
 })(jQuery);
 
-/**
-* Cette fonction va initialiser le plugin
-*/
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
+
 function initCoverflow(elementID)
 {
-	//Création du bloc Coverflow
+  //Création du bloc Coverflow
     $('<div id="coverflow-controls">').appendTo(elementID);
-	
-	//Ajout des controles (stop/play, next ...)
-	$('<button class="coverflow-control-elmt stopestart">Démarrer</button>').appendTo(elementID + " #coverflow-controls");
-}
- 
-/**
-* Liaison avec les evenements
-*/
-function handleEvt(elementID)
-{
-    $(elementID + ' .stopestart').on('click', function () {
-        alert("play/stop on instance " + elementID);
-    });
+
+  
+  //Ajout des controles (stop/play, next ...)
+  $('<button class="coverflow-control-elmt stopestart">Demarrer</button>').appendTo(elementID + " #coverflow-controls");
 }
